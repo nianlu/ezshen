ef11e6ed3fa263eb9d44551c24eec1310012768d
nianlu <nianlushu@gmail.com>
2019-08-06 00:26:39 -0400

    basic

--- client/src/container/Body.js
+++ client/src/container/Body.js
@@ -3,22 +3,32 @@ import React from 'react';
 function Body() {
   return (
     <div className="container">
-      <div class="divider"></div>
-      <div class="section">
-        <h5>Section 1</h5>
-        <p>Stuff</p>
+      <div className="row">
+        <div className="col s4">
+          <h2>你的未来从这里开始</h2>
+        </div>
+        <div className="col s12">
+          <p>无论你是不是第一次申请，我们都会帮助你作出最好的选择！</p>
+          <a className="waves-effect waves-light btn">现在申请</a>
+        </div>
       </div>
-      <div class="divider"></div>
-      <div class="section">
-        <h5>Section 2</h5>
-        <p>Stuff</p>
+          <img className="responsive-img" src="./college.jpg"></img>
+      <div className="divider"></div>
+      <div className="row">
+        <div className="col">
+          <h4>探索所有的学习，获取所有的资源</h4>
+          <input placeholder="学校，城市 或 专业"></input>
+          <p>添加标签: 
+          <div className="chip">公立学校<i className="close material-icons">x</i></div>
+          <div className="chip">私立学校<i className="close material-icons">x</i></div>
+          <div className="chip">排名前100<i className="close material-icons">x</i></div>
+          <div className="chip">不需要GRE<i className="close material-icons">x</i></div>
+          </p>
+          <a className="waves-effect waves-light btn">搜索</a>
+          <span> </span>
+          <a>查看所有</a>
+        </div>
       </div>
-      <div class="divider"></div>
-      <div class="section">
-        <h5>Section 3</h5>
-        <p>Stuff</p>
-      </div>
-      <a class="waves-effect waves-light btn">button</a>
     </div>
   );
 }
--- client/src/container/Nav.js
+++ client/src/container/Nav.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className=" teal lighten-3">
       <div className="nav-wrapper container">
-        <a href="#" className="brand-logo" style={{paddingLeft: '1rem'}}>ezshen</a>
+        <a href="#" className="brand-logo" style={{paddingLeft: '1rem'}}>易申</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="#">查询</a></li>
           <li><a href="#">计划</a></li>
