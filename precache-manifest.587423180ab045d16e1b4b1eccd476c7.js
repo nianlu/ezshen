1024bc79a4c4ae3be5799388895b14905a112dc5
nianlu <nianlushu@gmail.com>
2019-08-05 21:41:17 -0400

    Updates

---
+++ precache-manifest.587423180ab045d16e1b4b1eccd476c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95cc48bd152a05721352ebf5b2d0fe7",
+    "url": "/ezshen/index.html"
+  },
+  {
+    "revision": "de722a758df853a97f59",
+    "url": "/ezshen/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "de722a758df853a97f59",
+    "url": "/ezshen/static/js/2.a529c34e.chunk.js"
+  },
+  {
+    "revision": "e55b680b91223d88fdc1",
+    "url": "/ezshen/static/js/main.9f335cb2.chunk.js"
+  },
+  {
+    "revision": "43bfd6586a428083411d",
+    "url": "/ezshen/static/js/runtime~main.0a60d8d2.js"
+  }
+]);
